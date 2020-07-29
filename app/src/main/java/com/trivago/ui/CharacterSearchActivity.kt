f4e5bdc00240d8e49c25215c422a333b1700b29b
Lemmah <lemayiannakolah@gmail.com>
2020-07-30 02:20:14 +0300

    setting up character search and details screen

---
+++ app/src/main/java/com/trivago/ui/CharacterSearchActivity.kt
@@ -0,0 +1,12 @@
+package com.trivago.ui
+
+import android.os.Bundle
+import com.trivago.R
+
+class CharacterSearchActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_character_search)
+    }
+}
