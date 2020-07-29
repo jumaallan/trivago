8d73e06a3aed9d0d6b0c41d4f0e09641e515fd35
Lemmah <lemayiannakolah@gmail.com>
2020-07-30 01:37:33 +0300

    a little housekeeping

--- app/src/main/java/com/trivago/ui/MainActivity.kt
+++ app/src/main/java/com/trivago/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.trivago
+package com.trivago.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.trivago.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
