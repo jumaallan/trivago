01d171d25ea1cc1ca9d45d0f13523b57130fc5af
Juma Allan <allanjuma@gmail.com>
2020-07-30 21:51:09 +0300

    setting up app splash page

---
+++ app/src/main/java/com/trivago/ui/views/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.trivago.ui.views
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import com.trivago.R
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(applicationContext, CharacterSearchActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
