52ceae19226dbbff0fb9fe598e90dcde5367c07d
Juma Allan <allanjuma@gmail.com>
2020-07-31 22:59:30 +0300

    setting up character vm and repo

--- app/src/main/java/com/trivago/ui/viewmodel/CharacterDetailsViewModel.kt
+++ app/src/main/java/com/trivago/ui/viewmodel/CharacterDetailsViewModel.kt
@@ -1,1 +1,8 @@
 package com.trivago.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.trivago.data.repository.CharacterDetailsRepository
+
+class CharacterDetailsViewModel(
+    private val characterDetailsRepository: CharacterDetailsRepository
+) : ViewModel()
