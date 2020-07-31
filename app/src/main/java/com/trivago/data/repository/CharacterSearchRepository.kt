eead41f0eb409b54973ec81c7e91a3810256735c
Juma Allan <allanjuma@gmail.com>
2020-07-31 22:50:21 +0300

    setting up character search repo

--- app/src/main/java/com/trivago/data/repository/CharacterSearchRepository.kt
+++ app/src/main/java/com/trivago/data/repository/CharacterSearchRepository.kt
@@ -1,1 +1,9 @@
 package com.trivago.data.repository
+
+import com.trivago.core.api.StarWarsAPI
+import com.trivago.data.dao.CharacterDao
+
+class CharacterSearchRepository(
+    private val starWarsAPI: StarWarsAPI,
+    private val characterDao: CharacterDao
+)
