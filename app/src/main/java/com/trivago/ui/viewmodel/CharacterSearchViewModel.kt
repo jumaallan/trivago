97e5f14ec9771354af5974cb791655767846d2b6
Juma Allan <allanjuma@gmail.com>
2020-07-31 22:50:33 +0300

    setting up character search viewmodel

--- app/src/main/java/com/trivago/ui/viewmodel/CharacterSearchViewModel.kt
+++ app/src/main/java/com/trivago/ui/viewmodel/CharacterSearchViewModel.kt
@@ -1,1 +1,8 @@
 package com.trivago.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.trivago.data.repository.CharacterSearchRepository
+
+class CharacterSearchViewModel(
+    private val characterSearchRepository: CharacterSearchRepository
+) : ViewModel()
