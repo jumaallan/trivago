cbde2df22cc8bae7e4d46a348a6174516eb6865d
Juma Allan <allanjuma@gmail.com>
2020-08-01 01:04:00 +0300

    updated character details

--- app/src/main/java/com/trivago/ui/views/CharacterDetailsActivity.kt
+++ app/src/main/java/com/trivago/ui/views/CharacterDetailsActivity.kt
@@ -1,12 +1,26 @@
 package com.trivago.ui.views
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.trivago.R
 
-class CharacterDetailsActivity : AppCompatActivity() {
+class CharacterDetailsActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_character_details)
     }
+
+    companion object {
+
+        fun createIntent(context: Context, cardName: String): Intent {
+            return Intent(context, CharacterDetailsActivity::class.java).apply {
+                putExtra(CHARACTER_NAME, cardName)
+            }
+        }
+
+        private const val CHARACTER_NAME = "characterName"
+    }
+
 }
