aebabb6d8a1a46ddf9de0e95b4de69ffb8267171
Juma Allan <allanjuma@gmail.com>
2020-08-01 16:15:29 +0300

    disable Fts3

--- app/src/main/java/com/trivago/data/model/Character.kt
+++ app/src/main/java/com/trivago/data/model/Character.kt
@@ -4,9 +4,12 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
+//@Fts3 // FTS3 and FTS4 are SQLite virtual table modules that allows users to perform full-text searches on a set of documents - https://www.sqlite.org/fts3.html || https://developer.android.com/reference/androidx/room/Fts3
 data class Character(
     @PrimaryKey
-    var id: Long,
+//    @Ignore
+//    @ColumnInfo(name = "rowid")
+    var rowId: Int,
     var name: String,
     var birthYear: String,
     var height: String,
