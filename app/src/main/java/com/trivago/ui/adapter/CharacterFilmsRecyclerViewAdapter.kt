922237375e4dc2172ba2543cc21cbefd44e2668a
Juma Allan <allanjuma@gmail.com>
2020-08-01 20:32:47 +0300

    setting up character films view and recyclerview

---
+++ app/src/main/java/com/trivago/ui/adapter/CharacterFilmsRecyclerViewAdapter.kt
@@ -0,0 +1,47 @@
+package com.trivago.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.trivago.core.data.models.FilmDetailResponse
+import com.trivago.databinding.ItemFilmBinding
+
+object CharacterFilmsDiffer : DiffUtil.ItemCallback<FilmDetailResponse>() {
+    override fun areItemsTheSame(
+        oldItem: FilmDetailResponse,
+        newItem: FilmDetailResponse
+    ): Boolean =
+        oldItem.title == newItem.title
+
+    override fun areContentsTheSame(
+        oldItem: FilmDetailResponse,
+        newItem: FilmDetailResponse
+    ): Boolean = oldItem == newItem
+}
+
+internal class CharacterFilmsRecyclerViewAdapter :
+    ListAdapter<FilmDetailResponse, CharacterFilmsRecyclerViewAdapter.ViewHolder>(
+        CharacterFilmsDiffer
+    ) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemFilmBinding.inflate(inflater)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(private val binding: ItemFilmBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(filmDetailResponse: FilmDetailResponse) {
+            binding.film = filmDetailResponse
+            binding.executePendingBindings()
+        }
+    }
+}
