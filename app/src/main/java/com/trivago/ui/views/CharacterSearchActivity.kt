bf02cb00503766565799755c8edd83af94ee1829
Juma Allan <allanjuma@gmail.com>
2020-08-01 16:16:51 +0300

    setting up search configuration

--- app/src/main/java/com/trivago/ui/views/CharacterSearchActivity.kt
+++ app/src/main/java/com/trivago/ui/views/CharacterSearchActivity.kt
@@ -1,6 +1,11 @@
 package com.trivago.ui.views
 
+import android.app.SearchManager
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.lifecycleScope
 import com.trivago.R
@@ -14,6 +19,7 @@ import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import timber.log.Timber
 
 class CharacterSearchActivity : BaseActivity() {
 
@@ -26,6 +32,13 @@ class CharacterSearchActivity : BaseActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_character_search)
         binding.lifecycleOwner = this
         binding.characterSearchViewModel = characterSearchViewModel
+
+        if (Intent.ACTION_SEARCH == intent.action) {
+            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
+                // characterSearchViewModel.searchStarWarsCharacters(query)
+                Timber.d("Juma $query")
+            }
+        }
 
         charactersRecyclerViewAdapter = CharactersRecyclerViewAdapter {
             val intent = CharacterDetailsActivity.createIntent(
@@ -53,5 +66,18 @@ class CharacterSearchActivity : BaseActivity() {
             binding.recyclerViewCharacters.show()
             charactersRecyclerViewAdapter.submitList(charactersList)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu.findItem(R.id.menu_search).actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+            setIconifiedByDefault(false)
+        }
+
+        return true
     }
 }
