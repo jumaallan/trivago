16dcc7bd6cffa62b596e6dc66364ded1e154b981
Juma Allan <allanjuma@gmail.com>
2020-08-01 13:01:15 +0300

    setting up character details viewmodel

--- app/src/main/java/com/trivago/ui/viewmodel/CharacterDetailsViewModel.kt
+++ app/src/main/java/com/trivago/ui/viewmodel/CharacterDetailsViewModel.kt
@@ -1,8 +1,28 @@
 package com.trivago.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.trivago.core.models.*
 import com.trivago.data.repository.CharacterDetailsRepository
 
 class CharacterDetailsViewModel(
     private val characterDetailsRepository: CharacterDetailsRepository
-) : ViewModel()
+) : ViewModel() {
+
+    suspend fun fetchSpecies(characterUrl: String): SpeciesResponse =
+        characterDetailsRepository.fetchSpecies(characterUrl)
+
+    suspend fun fetchSpeciesDetails(speciesUrl: String): SpeciesDetailResponse =
+        characterDetailsRepository.fetchSpeciesDetails(speciesUrl)
+
+    suspend fun fetchFilms(characterUrl: String): FilmsResponse =
+        characterDetailsRepository.fetchFilms(characterUrl)
+
+    suspend fun fetchFilmDetails(filmsUrl: String): FilmDetailResponse =
+        characterDetailsRepository.fetchFilmDetails(filmsUrl)
+
+    suspend fun fetchPlanet(characterUrl: String): PlanetResponse =
+        characterDetailsRepository.fetchPlanet(characterUrl)
+
+    suspend fun fetchPlanetDetails(planetUrl: String): PlanetDetailsResponse =
+        characterDetailsRepository.fetchPlanetDetails(planetUrl)
+}
