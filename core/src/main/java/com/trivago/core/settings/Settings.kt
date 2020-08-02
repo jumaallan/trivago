932c62ac1ddbf5134ebf1378ed969379520d1aa0
Juma Allan <allanjuma@gmail.com>
2020-08-02 15:16:07 +0300

    documenting core settings package

--- core/src/main/java/com/trivago/core/settings/Settings.kt
+++ core/src/main/java/com/trivago/core/settings/Settings.kt
@@ -3,18 +3,37 @@ package com.trivago.core.settings
 import android.content.SharedPreferences
 import androidx.core.content.edit
 
+/**
+ *  Shared Preferences Settings
+ *
+ *  This class manages shared preferences globally, for the app and any other module that's introduced
+ *
+ *  @param settings
+ *  @constructor creates an instance of the Settings class
+ */
 class Settings(
     private val settings: SharedPreferences
 ) {
 
+    /**
+     * Fetches the saved bearer token
+     * @return the bearer token, as a nullable string
+     */
     fun getBearerToken(): String? = settings.getString(SettingsConstants.BEARER_TOKEN_KEY, "")
 
+    /**
+     * Saves the bearer token
+     * @param token
+     */
     fun setBearerToken(token: String) {
         settings.edit {
             putString(SettingsConstants.BEARER_TOKEN_KEY, token)
         }
     }
 
+    /**
+     * Responsible for clearing the shared preferences file
+     */
     fun clearData() {
         settings.edit { clear() }
     }
