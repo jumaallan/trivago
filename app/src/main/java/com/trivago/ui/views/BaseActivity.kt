400e81d6f08b7cabac4139a151cfeea4e2f6ed0c
Juma Allan <allanjuma@gmail.com>
2020-08-02 20:40:11 +0300

    setting up theme changes, inside base activity

--- app/src/main/java/com/trivago/ui/views/BaseActivity.kt
+++ app/src/main/java/com/trivago/ui/views/BaseActivity.kt
@@ -1,5 +1,36 @@
 package com.trivago.ui.views
 
+import android.os.Build
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.Observer
+import com.trivago.ui.viewmodel.ThemeViewModel
+import org.koin.android.ext.android.inject
 
-open class BaseActivity : AppCompatActivity()
+open class BaseActivity : AppCompatActivity() {
+
+    private val themeViewModel: ThemeViewModel by inject()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        themeViewModel.getAppTheme().observe(
+            this,
+            Observer {
+                it.let {
+                    when (it) {
+                        "Light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        "Dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        "System" ->
+                            if (Build.VERSION.SDK_INT >= 29) {
+                                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                            } else {
+                                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                            }
+                    }
+                }
+            }
+        )
+    }
+}
