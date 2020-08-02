a57df344d8224384b06fbcbb7b39597b73515bed
Juma Allan <allanjuma@gmail.com>
2020-08-03 00:54:29 +0300

    added a few unit tests on core module

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -66,5 +66,5 @@ dependencies {
     // tests
     testImplementation(TestLibraries.jUnit)
     androidTestImplementation(TestLibraries.jUnitTest)
-    androidTestImplementation(TestLibraries.espresso)
+    testImplementation(TestLibraries.truth)
 }
