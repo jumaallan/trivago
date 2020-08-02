897502a1e8f25a92df8772b9f2e0372ecc40b4ef
Juma Allan <allanjuma@gmail.com>
2020-08-02 04:15:08 +0300

    exposing data as livedata on viewmodel

--- app/src/main/java/com/trivago/ui/viewmodel/CharacterDetailsViewModel.kt
+++ app/src/main/java/com/trivago/ui/viewmodel/CharacterDetailsViewModel.kt
@@ -1,13 +1,14 @@
 package com.trivago.ui.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.trivago.core.data.models.Film
 import com.trivago.core.data.models.Planet
 import com.trivago.core.data.models.Species
 import com.trivago.data.repository.CharacterFilmsRepository
 import com.trivago.data.repository.CharacterPlanetRepository
 import com.trivago.data.repository.CharacterSpeciesRepository
-import kotlinx.coroutines.flow.Flow
 
 class CharacterDetailsViewModel(
     private val characterSpeciesRepository: CharacterSpeciesRepository,
@@ -15,12 +16,12 @@ class CharacterDetailsViewModel(
     private val characterPlanetRepository: CharacterPlanetRepository
 ) : ViewModel() {
 
-    suspend fun fetchSpecies(characterUrl: String): Flow<List<Species>> =
-        characterSpeciesRepository.fetchSpecies(characterUrl)
+    suspend fun fetchSpecies(characterUrl: String): LiveData<List<Species>> =
+        characterSpeciesRepository.fetchSpecies(characterUrl).asLiveData()
 
-    suspend fun fetchFilms(characterUrl: String): Flow<List<Film>> =
-        characterFilmsRepository.fetchFilms(characterUrl)
+    suspend fun fetchFilms(characterUrl: String): LiveData<List<Film>> =
+        characterFilmsRepository.fetchFilms(characterUrl).asLiveData()
 
-    suspend fun fetchPlanet(characterUrl: String): Flow<Planet> =
-        characterPlanetRepository.fetchPlanet(characterUrl)
+    suspend fun fetchPlanet(characterUrl: String): LiveData<Planet> =
+        characterPlanetRepository.fetchPlanet(characterUrl).asLiveData()
 }
