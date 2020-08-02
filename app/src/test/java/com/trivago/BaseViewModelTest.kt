78faf09a3b5bab822b71b1f9b1cb60828923884f
Juma Allan <allanjuma@gmail.com>
2020-08-03 02:59:32 +0300

    setting up baseviewmodel test class

---
+++ app/src/test/java/com/trivago/BaseViewModelTest.kt
@@ -0,0 +1,25 @@
+package com.trivago
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.trivago.util.CoroutineTestRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
+import org.koin.test.AutoCloseKoinTest
+
+abstract class BaseViewModelTest : AutoCloseKoinTest() {
+
+    /**
+     * Swaps the background executor used by the Architecture Components with a different one which
+     * executes each task synchronously.
+     **/
+    @get:Rule
+    open val instantExecutorRule = InstantTaskExecutorRule()
+
+    /**
+     * A test rule to allow testing coroutines that use the main dispatcher
+     */
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    open val coroutineTestRule = CoroutineTestRule()
+
+}
