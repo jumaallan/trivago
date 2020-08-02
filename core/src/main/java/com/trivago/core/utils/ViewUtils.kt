0f0774f74f96db15f7bac62e20a500f60e906b38
Juma Allan <allanjuma@gmail.com>
2020-08-02 17:47:56 +0300

    a little housekeeping

--- core/src/main/java/com/trivago/core/utils/ViewUtils.kt
+++ core/src/main/java/com/trivago/core/utils/ViewUtils.kt
@@ -2,23 +2,40 @@ package com.trivago.core.utils
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Color
+import android.os.Build
 import android.os.IBinder
 import android.view.View
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.core.content.getSystemService
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.flatMapConcat
-import kotlinx.coroutines.flow.toList
 
 /**
  * View Utils
  *
  * These are various extension functions used across the application. Easily re-usable
  */
+
+/**
+ * Responsible for making the status bar transparent
+ */
+fun Activity.makeStatusBarTransparent() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        window.apply {
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            } else {
+                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            }
+            statusBarColor = Color.TRANSPARENT
+        }
+    }
+}
 
 /**
  * Responsible for dismissing the keyboard
@@ -81,13 +98,3 @@ fun String.toHttps(): String =
     if (!this.contains("https")) {
         this.replace("http", "https")
     } else this
-
-/**
- * Responsible for converting Flow<List<T>> to a List<>
- *
- * @param T
- * @return a List from a Flow<List<>>
- */
-@FlowPreview
-suspend fun <T> Flow<List<T>>.flattenToList() =
-    flatMapConcat { it.asFlow() }.toList()
