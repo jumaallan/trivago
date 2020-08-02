3d1199cbe5e208698251de61ea0ce1a2a4d78d50
Juma Allan <allanjuma@gmail.com>
2020-08-02 20:47:05 +0300

    setting up ResponseToData - mapping extension functions

---
+++ core/src/main/java/com/trivago/core/data/mappers/ResponseToData.kt
@@ -0,0 +1,34 @@
+package com.trivago.core.data.mappers
+
+import com.trivago.core.data.models.Film
+import com.trivago.core.data.models.Planet
+import com.trivago.core.data.models.Species
+import com.trivago.core.data.models.StarWarsCharacter
+import com.trivago.core.data.network.CharacterResponse
+import com.trivago.core.data.network.FilmDetailResponse
+import com.trivago.core.data.network.PlanetDetailsResponse
+import com.trivago.core.data.network.SpeciesDetailResponse
+
+/**
+ * Responsible for mapping the FilmDetailResponse to Film
+ */
+fun FilmDetailResponse.toResponse(): Film =
+    Film(this.title, this.openingCrawl)
+
+/**
+ * Responsible for mapping the PlanetDetailsResponse to Planet
+ */
+fun PlanetDetailsResponse.toResponse(): Planet =
+    Planet(this.name, this.population)
+
+/**
+ * Responsible for mapping the CharacterResponse to StarWarsCharacter
+ */
+fun CharacterResponse.toResponse(): StarWarsCharacter =
+    StarWarsCharacter(this.name, this.birthYear, this.height, this.url)
+
+/**
+ * Responsible for mapping the SpeciesDetailResponse to Species
+ */
+fun SpeciesDetailResponse.toResponse(): Species =
+    Species(this.name, this.language)
