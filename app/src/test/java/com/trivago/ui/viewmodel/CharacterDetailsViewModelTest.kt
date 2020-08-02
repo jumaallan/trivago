177b4806c3a590cc789364cafb6e97130a992093
Juma Allan <allanjuma@gmail.com>
2020-08-03 02:37:35 +0300

    setting up view models unit test

---
+++ app/src/test/java/com/trivago/ui/viewmodel/CharacterDetailsViewModelTest.kt
@@ -0,0 +1,19 @@
+package com.trivago.ui.viewmodel
+
+import org.junit.Before
+import org.junit.Test
+
+class CharacterDetailsViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getCharacterResponseState() {
+    }
+
+    @Test
+    fun getCharacterDetails() {
+    }
+}
---
+++ app/src/test/java/com/trivago/ui/viewmodel/CharacterSearchViewModelTest.kt
@@ -0,0 +1,23 @@
+package com.trivago.ui.viewmodel
+
+import org.junit.Before
+import org.junit.Test
+
+class CharacterSearchViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun searchStarWarsCharacters() {
+    }
+
+    @Test
+    fun saveCharacter() {
+    }
+
+    @Test
+    fun getCharacters() {
+    }
+}
