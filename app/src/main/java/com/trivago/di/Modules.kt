a968f3e026f369dc6b4a0bc4958d22a5be4f2646
Juma Allan <allanjuma@gmail.com>
2020-08-02 15:23:36 +0300

    documenting app DI package

--- app/src/main/java/com/trivago/di/Modules.kt
+++ app/src/main/java/com/trivago/di/Modules.kt
@@ -13,6 +13,20 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
+/**
+ *  App DI Module
+ *
+ *  This file is responsible for setting up and injecting the following
+ *      - Database (Room DB)
+ *      - DAOs
+ *      - Repository
+ *      - ViewModels
+ *
+ * The modules are only available on the app module. They are app specific and therefore should
+ * be managed centrally inside the app module.
+ *
+ * This setup also allows us to re-use the globally injected modules, to avoid duplication.
+ */
 private val databaseModule: Module = module {
     single {
         Room.databaseBuilder(
