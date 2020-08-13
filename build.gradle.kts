bfbba21951ca408e2466e5a55702d331ddae4723
Harun Wangereka <harunwangereka@gmail.com>
2020-08-13 15:49:41 +0300

    Added dependencies update plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     id(BuildPlugins.kotlinAndroid) apply false
     id(BuildPlugins.kotlinAndroidExtensions) apply false
     id(BuildPlugins.dokkaPlugin) version Versions.dokka
+    id(BuildPlugins.gradleVersionsPlugin) version Versions.gradleVersionsPlugin
     jacoco
 }
 
