1b2d6f7555be704e3d4b5afe8f1b09e465d8cdf5
Juma Allan <allanjuma@gmail.com>
2020-08-19 01:28:38 +0300

    cleaning up the dao test >> move models to sample data

--- app/src/test/java/com/trivago/data/dao/CharacterDaoTest.kt
+++ app/src/test/java/com/trivago/data/dao/CharacterDaoTest.kt
@@ -2,7 +2,7 @@ package com.trivago.data.dao
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.trivago.BaseTest
-import com.trivago.data.model.Character
+import com.trivago.data.sample.testCharacters
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runBlockingTest
@@ -22,11 +22,6 @@ internal class CharacterDaoTest : BaseTest() {
     @ExperimentalCoroutinesApi
     @Test
     fun `test inserting and retrieving characters`() = runBlockingTest {
-        val testCharacters = listOf(
-            Character("Darth Vader", "", "", ""),
-            Character("Yoda", "", "", ""),
-            Character("Biggs Dark", "", "", "")
-        )
         characterDao.insert(testCharacters)
         val character = characterDao.getCharacters().first().toList()[0]
         assertThat(character.name, `is`(testCharacters[0].name))
