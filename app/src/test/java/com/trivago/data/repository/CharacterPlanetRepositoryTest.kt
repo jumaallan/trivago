d0d67e2f6d70f68e5207174a94538236aa3faad5
Juma Allan <allanjuma@gmail.com>
2020-08-20 00:11:45 +0300

    updated the repository test

--- app/src/test/java/com/trivago/data/repository/CharacterPlanetRepositoryTest.kt
+++ app/src/test/java/com/trivago/data/repository/CharacterPlanetRepositoryTest.kt
@@ -3,6 +3,7 @@ package com.trivago.data.repository
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth
 import com.trivago.BaseTest
+import com.trivago.core.network.NetworkResult
 import com.trivago.dispatcher.MockRequestDispatcher
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
@@ -23,9 +24,11 @@ internal class CharacterPlanetRepositoryTest : BaseTest() {
     @Test
     fun `test fetching planet using an existing character url`() {
         runBlocking {
-            val planet =
+            val planetResponse =
                 characterPlanetRepository.fetchPlanet(MockRequestDispatcher.EXISTING_CHARACTER_URL)
-            Truth.assertThat(planet?.name).matches("Tatooine")
+            Truth.assertThat(planetResponse).isInstanceOf(NetworkResult.Success::class.java)
+            (planetResponse as NetworkResult.Success)
+            Truth.assertThat(planetResponse.data.name).matches("Tatooine")
         }
     }
 }
