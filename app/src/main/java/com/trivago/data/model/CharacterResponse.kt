115a38f811c57b57cf375d01fb09ff1f1d9e8036
Juma Allan <allanjuma@gmail.com>
2020-08-20 00:13:29 +0300

    pass sealed class to CharacterResponse.kt

--- app/src/main/java/com/trivago/data/model/CharacterResponse.kt
+++ app/src/main/java/com/trivago/data/model/CharacterResponse.kt
@@ -3,9 +3,10 @@ package com.trivago.data.model
 import com.trivago.core.data.models.Film
 import com.trivago.core.data.models.Planet
 import com.trivago.core.data.models.Species
+import com.trivago.core.network.NetworkResult
 
 data class CharacterResponse(
-    val planet: Planet?,
+    val planet: NetworkResult<Planet?>?,
     val films: List<Film>?,
     val species: List<Species>?
 )
